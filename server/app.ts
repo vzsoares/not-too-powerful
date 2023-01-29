07c7c6a9f0319fdeefb6e5e6761317ad2b7dcdec
vzsoares <jogolots2@gmail.com>
2023-01-29 20:12:31 -0300

    add safety methods .server

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,27 @@
+import path from 'path';
+
+import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimiter from 'express-rate-limit';
+import cookieParser from 'cookie-parser';
+dotenv.config();
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT ?? 4000;
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  }),
+);
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get('/', (req, res) => {
   res.send(JSON.stringify({ name: 'Hello World!' }));
