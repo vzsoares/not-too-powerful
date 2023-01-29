9937fadad4c15c6d382d681bca0699054f03a92d
vzsoares <jogolots2@gmail.com>
2023-01-29 18:37:58 -0300

    enh lint rules

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,16 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(JSON.stringify({ name: "Hello World!" }));
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({ name: 'Hello World!' }));
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send("404");
+app.all('*', (req, res) => {
+  res.status(404).send('404');
 });
 
 app.listen(port, () => {
