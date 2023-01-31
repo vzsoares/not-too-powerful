2340c2981dc39f6600f65fd9e994f4ba91c8fd58
vzsoares <jogolots2@gmail.com>
2023-01-30 22:24:13 -0300

    enh middlewares

--- server/app.ts
+++ server/app.ts
@@ -16,6 +16,7 @@ const port = process.env.PORT ?? 4000;
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
 app.use(
