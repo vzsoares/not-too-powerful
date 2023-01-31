3f83134ddb3fcd20d1f83f7e8daa263ce81f10bf
vzsoares <jogolots2@gmail.com>
2023-01-30 22:12:04 -0300

    enh lint rules

--- server/app.ts
+++ server/app.ts
@@ -6,12 +6,15 @@ import helmet from 'helmet';
 import cors from 'cors';
 import rateLimiter from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
+
+import notFound from '@middleware/not-found';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 4000;
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -23,13 +26,15 @@ app.use(
 );
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.get('/', (req, res) => {
-  res.send(JSON.stringify({ name: 'Hello World!' }));
+app.get('/api/v1', (req, res) => {
+  res.send('ok');
 });
 
-app.all('*', (req, res) => {
-  res.status(404).send('404');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
+
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
