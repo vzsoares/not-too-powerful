44504f76c5d8c93694fcf885b7dc8d1608ec69f4
vzsoares <jogolots2@gmail.com>
2023-01-31 22:36:22 -0300

    enh husky pre-push

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vitest/config';
+import { mergeConfig } from 'vite';
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./.test/setup.ts'],
-    include: ['**/(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
-  },
-});
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./.test/setup.ts'],
+      include: ['**/(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    },
+  }),
+);
