de64d2733a80d99d74e90cef0234a43dfd695c60
vzsoares <jogolots2@gmail.com>
2023-02-28 21:52:13 -0300

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,31 @@
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+
 function App(): JSX.Element {
-  return <div className='App'>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+function Home() {
+  return <p>Home</p>;
+}
+function Error() {
+  return <p>Error</p>;
+}
+
+function Layout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
