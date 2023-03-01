80f6b088ea3e40c2dcd8361ce27d444f9e744f94
vzsoares <jogolots2@gmail.com>
2023-02-28 21:55:42 -0300

    plug redux provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,31 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import store, { persistor } from './store';
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='*' element={<Error />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={<SplashLoading />} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='*' element={<Error />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
 export default App;
+
+function SplashLoading() {
+  return <p>loading...</p>;
+}
 
 function Home() {
   return <p>Home</p>;
