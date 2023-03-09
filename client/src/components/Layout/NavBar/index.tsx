7c295385da85a9f180c6c25224643c5bdee7fc4c
vzsoares <jogolots2@gmail.com>
2023-03-08 22:46:13 -0300

    feat log in/out

--- client/src/components/Layout/NavBar/index.tsx
+++ client/src/components/Layout/NavBar/index.tsx
@@ -1,16 +1,64 @@
-import { Button, IconButton, Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import { Button, IconButton, Popover, Typography } from '@mui/material';
+import { Box, darken } from '@mui/system';
 import PersonIcon from '@mui/icons-material/Person';
 import { useEffect, useState } from 'react';
 
 import { discordGetUserCode } from '../../../../env';
 import { useLazyGetTokenQuery } from '../../../api/auth.api';
-import { useAppDispatch } from '../../../hooks';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { setAuth } from '../../../store/user';
 
 function Navbar() {
-  const [externalPopup, setExternalPopup] = useState<Window | null>(null);
+  return (
+    <NavContainer>
+      <Button
+        variant='contained'
+        sx={{ py: { xs: '1rem', md: '1.5rem' }, height: 'fit-content' }}
+      >
+        <Typography
+          variant='h3'
+          sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}
+        >
+          Add To Your Server
+        </Typography>
+      </Button>
+      <UserPopOver>
+        <IconButton>
+          <Box
+            sx={{
+              bgcolor: 'primary.main',
+              borderRadius: '50%',
+              p: '10px',
+              display: 'flex',
+            }}
+          >
+            <PersonIcon sx={{ color: 'white.main', fontSize: '3rem' }} />
+          </Box>
+        </IconButton>
+      </UserPopOver>
+    </NavContainer>
+  );
+}
+
+export default Navbar;
+
+function UserPopOver({ children }: { children: React.ReactElement }) {
+  const [anchorEl, setAnchorEl] = useState<HTMLDivElement | null>(null);
+  const user = useAppSelector((s) => s.user);
   const dispatch = useAppDispatch();
+
+  const handleInputClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  const [externalPopup, setExternalPopup] = useState<Window | null>(null);
 
   const handlePopup = () => {
     const width = 500;
@@ -51,47 +99,88 @@ function Navbar() {
           .unwrap()
           .then((r) => {
             dispatch(setAuth(r.data));
+            // TODO success toast
           })
           .catch((e) => {
             // TODO
             console.error('error login in', e);
           });
-        console.warn(`The popup URL has URL code param = ${code}`);
       }
     }, 500);
   }, [externalPopup, dispatch, verifyToken]);
 
   return (
-    <NavContainer>
-      <Button
-        onClick={handlePopup}
-        variant='contained'
-        sx={{ py: { xs: '1rem', md: '1.5rem' }, height: 'fit-content' }}
-      >
-        <Typography
-          variant='h3'
-          sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}
-        >
-          Add To Your Server
-        </Typography>
-      </Button>
-      <IconButton>
-        <Box
-          sx={{
-            bgcolor: 'primary.main',
-            borderRadius: '50%',
-            p: '10px',
+    <Box>
+      <Box onClick={handleInputClick}>{children}</Box>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        PaperProps={{
+          sx: {
+            borderRadius: '8px',
+            p: 2,
+            backgroundColor: (x) => darken(x.palette.secondary.main, 0.2),
+            minHeight: '50px',
+            minWidth: '150px',
             display: 'flex',
-          }}
-        >
-          <PersonIcon sx={{ color: 'white.main', fontSize: '3rem' }} />
-        </Box>
-      </IconButton>
-    </NavContainer>
+            justifyContent: 'center',
+            flexDirection: 'column',
+          },
+        }}
+      >
+        {user.auth?.access_token ? (
+          <Typography
+            sx={{
+              fontSize: { xs: '1rem', md: '1rem' },
+              cursor: 'pointer',
+              '&:hover': {
+                filter: 'opacity(0.85)',
+              },
+              '&:active': {
+                filter: 'opacity(0.55)',
+              },
+            }}
+            onClick={() => {
+              dispatch({ type: 'LOGOUT' });
+              handleClose();
+            }}
+          >
+            Logout
+          </Typography>
+        ) : (
+          <Typography
+            onClick={() => {
+              handlePopup();
+              handleClose();
+            }}
+            sx={{
+              fontSize: { xs: '1rem', md: '1rem' },
+              cursor: 'pointer',
+              '&:hover': {
+                filter: 'opacity(0.85)',
+              },
+              '&:active': {
+                filter: 'opacity(0.55)',
+              },
+            }}
+          >
+            Login
+          </Typography>
+        )}
+      </Popover>
+    </Box>
   );
 }
-
-export default Navbar;
 
 function NavContainer({ children }: { children: React.ReactNode }) {
   return (
