8f3826cd93cc6d857167b6b0cd16b5477d65b7bd
vzsoares <jogolots2@gmail.com>
2023-03-08 21:51:14 -0300

    add file input

--- client/src/pages/Home/UploadBox/index.tsx
+++ client/src/pages/Home/UploadBox/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import ImageIcon from '@mui/icons-material/Image';
+import { useRef } from 'react';
 
 function UploadBox() {
   return (
@@ -26,27 +27,51 @@ function UploadBox() {
       <ImageIcon color='primary' sx={{ fontSize: '5rem' }} />
       <Typography mb='10%' mt='1rem' sx={{ userSelect: 'none' }}>
         Paste, drag or&nbsp;
-        <Typography
-          textAlign='center'
-          component='span'
-          color='primary'
-          sx={{
-            textDecoration: 'underline',
-            fontSize: { xs: '1rem', md: '1rem' },
-            cursor: 'pointer',
-            '&:hover': {
-              filter: 'opacity(0.85)',
-            },
-            '&:active': {
-              filter: 'opacity(0.55)',
-            },
-          }}
-        >
-          click
-        </Typography>
+        <ImportBtn />
       </Typography>
     </Box>
   );
 }
 
 export default UploadBox;
+
+function ImportBtn() {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      // console.log(e.target.files?.[0]);
+    }
+  };
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleClick = () => inputRef.current?.click();
+  return (
+    <>
+      <Typography
+        onClick={handleClick}
+        textAlign='center'
+        component='span'
+        color='primary'
+        sx={{
+          textDecoration: 'underline',
+          fontSize: { xs: '1rem', md: '1rem' },
+          cursor: 'pointer',
+          '&:hover': {
+            filter: 'opacity(0.85)',
+          },
+          '&:active': {
+            filter: 'opacity(0.55)',
+          },
+        }}
+      >
+        click
+      </Typography>
+      <input
+        ref={inputRef}
+        type='file'
+        id='upload-file'
+        style={{ display: 'none' }}
+        // accept='.pgn,.jpg,.jpeg'
+        onChange={handleChange}
+      />
+    </>
+  );
+}
