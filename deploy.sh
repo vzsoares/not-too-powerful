b29d10842f4d4a2f15478155e1c78721e6591ad0
vzsoares <jogolots2@gmail.com>
2023-03-22 23:47:25 -0300

    enh deploysh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+
+# bash -x ./deploy.sh 
+
+# !run docker with env file, and expose port 4000 to host(ME)
 # [zizmackrok@zizmackrok-ms7a33 server]$ docker run -p 4000:4000 --env-file .env nottoopowerful:latest
+
+# !rund bash inside image
 # [zizmackrok@zizmackrok-ms7a33 ~]$ docker run -it nottoopowerful:latest sh
 
-yarn vite build --emptyOutDir --outDir ../server/dist/ 
+# !docker build example command
+# > docker image build --pull --file '/home/zizmackrok/Desktop/Code/not-too-powerful/Dockerfile' --tag 'nottoopowerful:latest' --label 'com.microsoft.created-by=visual-studio-code' '/home/zizmackrok/Desktop/Code/not-too-powerful' <
+
+echo "building client"
+(cd client/ && yarn vite build --emptyOutDir --outDir ../server/dist/) 
+
+echo "building server"
+(cd server/ && yarn build) 
+
+echo "building docker image"
+docker image build --no-cache --pull --file Dockerfile --tag 'nottoopowerful:latest' .
+
+# TODO send to aws
